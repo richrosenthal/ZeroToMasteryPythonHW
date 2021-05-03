bf09eb80bba13cf58ea6242543ad879d340537e5
richrosenthal <>
2021-05-03 21:16:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Day 1 of 100 days of code
+#This is just for me and not original code. I need a repository to refer back to and see my progress
+#- RNR 5-3-21
+
+#Data Types Lesson 1 
+
+int   
+float
+bool
+str
+list 
+tuple 
+set 
+dict 
+
+#Classes -> custom types 
+
+#Specialized Data Types 
+
+None 
